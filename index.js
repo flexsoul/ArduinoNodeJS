d40879dd7971f6915b9a9543a54335f3c6ba67d9
flexsoul <al.makarov@inbox.ru>
2022-12-23 13:28:18 +0300

    first commit

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server)
 
-var serialport = new require('serialport'), // include the serialport package
+const { SerialPort } = require('serialport')
+const port = new SerialPort({ path: 'COM3', baudRate: 9600 })
 
-SerialPort = serialport.SerialPort,    // make a local instance of serial port package
-portName = process.argv[2], // retrieve the port name from the command line argument
-portConfig = {
- //baudRate: 9600,
-baudRate: 9600,
- //call myPort.on('data') when a newline is received:
-parser: new  serialport.parsers.readline('\n')
-};
+const { ReadlineParser } = require('serialport');
+const parser = new ReadlineParser();
 
+port.pipe(parser);
 
 app.engine('ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
@@ -25,11 +21,17 @@ app.get('/', function (req, res) {
   res.render('index')
 });
 
+// Switches the port into "flowing mode"
+parser.on('data', function (data) {
+  console.log('Data:', data.toString())
+})
+
 var buttonValue = 0;
 
 io.on('connection', function(socket){
     console.log('Connection to client established');
 
+    /*
     io.emit('clicked message', buttonValue);
 
     socket.on('clicked message', function(msg){
@@ -37,6 +39,7 @@ io.on('connection', function(socket){
             io.emit('clicked message', buttonValue);
             console.log('Received message from client!',msg);
     });
+    */
 
 
     socket.on('disconnect',function(){
